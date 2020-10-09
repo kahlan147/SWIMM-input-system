d0620427550884915a51f0fd89429ce2575cec09
Niels Verheijen <kahlan147@gmail.com>
2020-10-09 12:36:13 +0200

    SitVariable implementation

---
+++ Assets/Input/ScriptableObjects/Scripts/SitVariable.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+public enum SitState { Sitting, Standing }
+
+public class SitStateEvent : UnityEvent<SitState> { }
+
+[CreateAssetMenu(fileName = "SitStateVariable", menuName = "DataParsers/SitStateVariable", order = 1)]
+
+public class SitVariable : ScriptableObject
+{
+    #region Variables
+    [SerializeField]
+    private SitState InitialValue;
+
+    [NonSerialized]
+    public SitState RuntimeValue;
+
+    public SitStateEvent OnChange = new SitStateEvent();
+    #endregion
+
+    #region Event Methods
+    public void ChangeValue(SitState newValue)
+    {
+        RuntimeValue = newValue;
+        if (OnChange != null)
+        {
+            OnChange.Invoke(RuntimeValue);
+        }
+    }
+
+    public void AddListener(UnityAction<SitState> action)
+    {
+        OnChange.AddListener(action);
+    }
+
+    public void RemoveListener(UnityAction<SitState> action)
+    {
+        OnChange.RemoveListener(action);
+    }
+    #endregion
+
+    #region Serialization
+    public void OnAfterDeserialize()
+    {
+        RuntimeValue = InitialValue;
+    }
+
+    public void OnBeforeSerialize() { }
+    #endregion
+}
